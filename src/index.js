9fe669efe7dace70046c2642d68822f407de1c01
wuchien <fe19ng@gmail.com>
2017-06-22 11:35:00 +0800

    end of #4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import sum from "./sum";
-import "./image_viewer";
-
-console.log(sum(1, 2));
+const button = document.createElement("button");
+button.innerText = "Click me";
+button.onclick = () => {
+  System.import("./image_viewer").then(module => {
+    // console.log(module);
+    module.default();
+  });
+};
+document.body.appendChild(button);
